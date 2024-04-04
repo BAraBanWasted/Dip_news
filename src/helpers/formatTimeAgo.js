5bfee528bd93a60ca5db4fea5a999b008e11496a
Bylkob_Maksim <gan1953max@mail.ru>
2024-04-04 15:55:29 +0300

    add skeleton for banner and news

--- src/helpers/formatTimeAgo.js
+++ src/helpers/formatTimeAgo.js
@@ -1,22 +1,22 @@
 //Сравниваем дату и в зависимости от количесва времени которое прошло и показываем либо секунды назада либо часы либо дни 
-export const formatTimeAgo = (dateString) =>{
-    const now = new Date ();
+export const formatTimeAgo = (dateString) => {
+    const now = new Date();
     const date = new Date(dateString);
-    const secondPast = (now.getTime() - date.getTime())/1000;
+    const secondPast = (now.getTime() - date.getTime()) / 100000;
 
-    if (secondPast < 60){
-        return `${Math.floor(secondPast)}s ago`;
+    if (secondPast < 60) {
+        return `${Math.round(secondPast)}s ago`;
     }
-   
-    if (secondPast < 3600){
-        return `${Math.floor(secondPast)/60}m ago`;
+
+    if (secondPast < 3600) {
+        return `${Math.round(secondPast) / 60}m ago`;
     }
-    if (secondPast <= 86400){
-        return `${Math.floor(secondPast)/3600}h ago`;
+    if (secondPast <= 86400) {
+        return `${Math.round(secondPast) / 3600}h ago`;
     }
-    if (secondPast < 86400){
-        const day = `${Math.floor(secondPast)/86400}`;
-        return day ==1 ? `${day} day ago`: `${day} days ago`
+    if (secondPast > 86400) {
+        const day = `${Math.round(secondPast) / 86400}`;
+        return day === 1 ? `${day} day ago` : `${day} days ago`
     }
 
 };
